46762fcfa5f558a4e2f9019bf33bca8e237e012a
jeissonarcadio778 <jeissonbeltranporras@gmail.com>
2023-01-30 21:10:10 -0500

    sls remove

--- aws-start-project/handler.js
+++ aws-start-project/handler.js
@@ -1,11 +1,12 @@
 'use strict';
 
 module.exports.hello = async (event) => {
+  console.log('This is a log. Hello log');
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Hey again. How are u?! This is a modification =)',
+        message: 'Hey again. How are u?! This is a modification =). Hello World. I will fight for my dreams',
         input: event,
       },
       null,
